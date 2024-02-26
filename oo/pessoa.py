035c395a0c0857c6bd7aa673c96f9391f506f1d8
Mateus Lopes <mtslopes.dev@gmail.com>
2024-02-26 15:18:43 -0300

    criados atributos de instância

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self, nome = None, idade = 35):
+        self.nome = nome
+        self.idade = idade
+    
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+    
+if __name__ == '__main__':
+    p = Pessoa('Luciano')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome="Renzo"
+    print(p.nome)
